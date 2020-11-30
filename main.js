c6de742a1a42afd1333f614509a1e57a63da450b
TeamOne2020 <73768135+TeamOne2020@users.noreply.github.com>
2020-11-29 17:07:56 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const apiData = {
 url:  'https://us-restaurant-menus.p.rapidapi.com/restaurant/',
 id: '389069',
 field: 'menuitems',
-key: '?rapidapi-key=d794304216mshc8565ee2d31fe3fp109107jsne575c3d52ba2'
+key: '?rapidapi-key=d794304216mshc8565ee2d31fe3fp109107jsne575c3d52ba2',
+pageNum: '&page=1',
 
 }
 
@@ -12,9 +13,9 @@ key: '?rapidapi-key=d794304216mshc8565ee2d31fe3fp109107jsne575c3d52ba2'
 
  
 
-const {url,id, field, key} = apiData
+const {url,id, field, key, pageNum} = apiData
 
-const apiUrl = `${url}${id}/${field}${key}`
+const apiUrl = `${url}${id}/${field}${key}${pageNum}`
 console.log(apiUrl)
 
 fetch(apiUrl)
@@ -33,21 +34,34 @@ fetch(apiUrl)
     .then( (menu) => generateHtml(menu))
 
    const generateHtml = (data) => {
-        const html = `
-        <Restaurant>RestaurantName: ${data.result.data[5].restaurant_name}</div>
-        <div>Name: ${data.result.data[5].menu_item_name}</div>
-        <div>Description: ${data.result.data[5].menu_item_description}</div>
-         <div>Price: ${data.result.data[5].menu_item_pricing[0].priceString}</div>
+
+
+    let output = "";
+    console.log(data.result.data.length)
+    for(let i=0;i<data.result.data.length;i++) {
+        let next_item = data.result.data[i];
+        const next_html = `
+        <div class='restaurant'>
+        <div>Restaurant Name: ${next_item.restaurant_name}</div>
+        <div>Name: ${next_item.menu_item_name}</div>
+        <div>Description: ${next_item.menu_item_description}</div>
+         <div>Price: ${next_item.menu_item_pricing[0].priceString}</div>
+         <div>Subsection: ${next_item.subsection}</div>
+         </div>
+         `;
+         output += next_html;
+
+
+    }
+    
       
      
       
      
         
-        <div>Subsection: ${data.result.data[5].subsection}</div>
-        </div>
-        `
+       
 
         const menuDiv = document.querySelector('.menu')
-        menuDiv.innerHTML = html
+        menuDiv.innerHTML = output
 
     }
